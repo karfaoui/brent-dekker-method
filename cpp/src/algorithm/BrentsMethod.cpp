f709dbbb8f0ea649377c66bb26c74b4b6bbd861e
Kais Arfaoui <16261071+karfaoui@users.noreply.github.com>
2019-02-20 14:11:21 -0500

    Update BrentsMethod.cpp

--- cpp/src/algorithm/BrentsMethod.cpp
+++ cpp/src/algorithm/BrentsMethod.cpp
@@ -1,10 +1,3 @@
-/*
- * @Author: Kais Arfaoui 
- * @Date: 2019-02-16 10:15:11 
- * @Last Modified by:   karfaoui 
- * @Last Modified time: 2019-02-20 14:02:14 
- */
-
 #include <cmath>
 #include "BrentsMethod.hpp"
 
